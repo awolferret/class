c826d270d46cd995b8bde71318267433d0f5c8fa
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-22 21:32:06 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Player player = new Player(100,50,15);
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        public int Health;
+        public int Mana;
+        public int Damage;
+
+        public Player(int health, int mana, int damage)
+        {
+            Health = health;
+            Mana = mana;
+            Damage = damage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Здоровье: {Health}");
+            Console.WriteLine($"Мана: {Mana}");
+            Console.WriteLine($"Урон: {Damage}");
+        }
+    }
+}
